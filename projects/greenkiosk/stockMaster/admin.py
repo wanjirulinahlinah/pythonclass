e3e5c957a77b559ba7d682eb452f3a58767f9c25
wanjirulinahlinah <wainainalinahw@gmail.com>
2023-06-22 08:13:43 +0300

    changes

--- projects/greenkiosk/stockMaster/admin.py
+++ projects/greenkiosk/stockMaster/admin.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
 
-# Register your models here.
+    
 from django.contrib import admin
 from .models import Stock
 
 @admin.register(Stock)
 class StockAdmin(admin.ModelAdmin):
-    list_display = ['name', 'quantity']
+    list_display = ('name', 'quantity')
+    search_fields = ('name',) 
